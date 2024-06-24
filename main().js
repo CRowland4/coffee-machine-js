f9064d0d056113c0669139a62db46eb87cd45a55
Caleb Rowland <cjrowland4@gmail.com>
2024-06-24 09:44:41 -0500

    Complete stage 3

--- main().js
+++ main().js
@@ -7,13 +7,32 @@ const amountPerCup = {
 }
 
 
+
 function main() {
-    console.log("Write how many cups of coffee you will need:");
-    const cups = Number(input());
-    console.log(`For ${cups} cups of coffee you will need:`);
-    console.log(`${cups * amountPerCup.water} ml of water`);
-    console.log(`${cups * amountPerCup.milk} ml of milk`);
-    console.log(`${cups * amountPerCup.beans} g of coffee beans`);
+    let machine = {
+        water: Number(input("Write how many ml of water the coffee machine has:\n")),
+        milk: Number(input("Write how many ml of milk the coffee machine has:\n")),
+        beans: Number(input("Write how many grams of coffee beans the coffee machine has:\n")),
+    }
+
+    let cupsAvailable = calculateCupsAvailable(machine);
+    let cupsRequested = Number(input("Write how many cups of coffee you will need:\n"));
+    if (cupsRequested === cupsAvailable) {
+        console.log("Yes, I can make that amount of coffee");
+    } else if (cupsRequested < cupsAvailable) {
+        console.log(`Yes, I can make that amount of coffee (and even ${cupsAvailable - cupsRequested} more than that)`)
+    } else {
+        console.log(`No, I can make only ${cupsAvailable} cups of coffee`);
+    }
+}
+
+
+function calculateCupsAvailable(machine) {
+    const cupsWater = Math.floor(machine.water / amountPerCup.water);
+    const cupsMilk = Math.floor(machine.milk / amountPerCup.milk);
+    const cupsBeans = Math.floor(machine.beans / amountPerCup. beans);
+
+    return Math.min(cupsWater, cupsMilk, cupsBeans);
 }
 
 main();
